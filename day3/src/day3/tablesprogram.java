65d0f7427324451ae11d0afec80eaf21987f15e5
RushikeshBharitkar <rushikeshbharitkar05@gmail.com>
2025-07-02 19:07:04 +0530

    Add files via upload

---
+++ day3/src/day3/tablesprogram.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class tablesprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++){
+			System.out.println("the table of "+i);
+			for(int j=1; j<=10; j++){
+				
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			
+		}
+
+	}
+
+}
